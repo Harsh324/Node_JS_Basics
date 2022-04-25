9c985569bb4ead2b3b25174154936014c55d7fea
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 <harshravi324@gmail.com>
2022-04-25 10:31:20 +0530

    Form to csv app done

--- Form_to_Csv/routes/index.js
+++ Form_to_Csv/routes/index.js
@@ -5,7 +5,26 @@ var bodyparser = require('body-parser')
 
 var FS = require('fs');
 
-const Path = 'Form_to_Csv/public/Extra/File.csv';
+var Csv = require('fast-csv');
+
+const Path = '/home/harsh/Documents/Github_Personal/Node_JS_Basics/Form_to_Csv/public/Extra/File.csv';
+
+const Flag = false;
+
+
+(async function(err)
+  {
+	if(!FS.existsSync(Path))
+	{
+		FS.writeFileSync(Path, "name;post");
+		console.log("Yes");
+	}
+	else
+	{
+		console.log("File Present");
+	}
+
+})();
 
 
 var urlencodedParser = bodyparser.urlencoded({ extended: false })
@@ -18,59 +37,46 @@ router.get('/', function(req, res, next) {
 
 router.post('/index', urlencodedParser, function(req, res, next)
 {
-  //console.log(req.body[name])
-
-	// FS.stat(Path, function(err, stat)
-	// {
-		if (FS.existsSync(path)) {
-			// path exists
-			console.log("exists:", path);
-		} 
-		else 
-		{
-			console.log("DOES NOT exist:", path);
-		}
-			// if(err == null) 
-			// {
-			// 	console.log('File exists');
-			// 	// FS.writeFileSync(Path, req.body.name + ";" + req.body.post + "\n");
-			// 	var writeStream = FS.createWriteStream(Path);
-			// 	writeStream.write(req.body.name + ";" + req.body.post + "\n");
-			// 	writeStream.end();	
-
-			// } 
-			// else if(err.code == 'ENOENT') 
-			// {
-			// 	// file does not exist
-			// 	// FS.writeFileSync(Path, "name;post\n");
-			// 	// FS.writeFileSync(Path, req.body.name + ";" + req.body.post + "\n");
-			// 	var writeStream = FS.createWriteStream(Path);
-			// 	writeStream.write("name;post\n");
-			// 	writeStream.write(req.body.name + ";" + req.body.post + "\n");
-			// 	writeStream.end();
-
-			// }
-			// else
-			// {
-			// 	console.log('Some other error: ', err.code);
-			// }
-	// });
   console.log(req.body.name);
   console.log(req.body);
 
-  // try 
-  // {
-  //   if (FS.existsSync(Path)) 
-  //   {
-	// 		FS.writeFileSync(Path, req.body.name + ";" + req.body.post);
-  //   }
-  // } catch(err) {
-  //   console.error(err)
-	// 	FS.writeFileSync(Path, "name;post");
-	// 	FS.writeFileSync(Path, req.body.name + ";" + req.body.post);
-  // }
 
+  FS.appendFile(Path, "\n" + req.body.name + ";" + req.body.post , function(err)
+  {
+	  if(err)
+	  {
+		  console.log(err);
+	  }
+	  else
+	  {
+		  console.log("File updated");
+	  }
+  });
 
+//   (async function(err)
+//   {
+// 	if(!FS.existsSync(Path))
+// 	{
+// 		FS.mkdirSync(Path, function(err){
+// 			if(err)
+// 			{
+// 				console.log("Error in Mkdir");
+// 			}
+// 			else
+// 			{
+// 				console.log("Exists ");
+// 			}
+// 		});
+		
+// 	}
+// 	else
+// 	{
+// 		console.log("Failure");
+// 	}
+
+// 	console.log(req.body.name);
+//   	console.log(req.body);
+//   })();
 
   res.render('index', {});
 })
